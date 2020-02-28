b402ba2147c403b743e130bcf759ad76d6e1ce3a
Alex Brotman <alexbrotman@gmail.com>
2020-02-28 11:50:33 -0600

    Adds switchboard.js with notes for scenarios of usage

---
+++ src/switchboard.js
@@ -0,0 +1,4 @@
+// Scenarios
+// How to handle removing a swapped tel number when it was originally stored indefinitely
+// How to handle purging old url parameters when a new one is used
+//// Perhaps concat the url parameter with 'switchboard_' for easy identification
