a52efaa19d4195e0fe57cb7385757ab1bf22083c
Alex Brotman <alexbrotman@gmail.com>
2020-02-28 12:01:50 -0600

    Adds DOM ready check

--- src/switchboard.js
+++ src/switchboard.js
@@ -2,3 +2,9 @@
 // How to handle removing a swapped tel number when it was originally stored indefinitely
 // How to handle purging old url parameters when a new one is used
 //// Perhaps concat the url parameter with 'switchboard_' for easy identification
+
+document.onreadystatechange = function() {
+  if (document.readyState === "complete") {
+    console.log("DOM loaded");
+  }
+};
