56f96f631b5440b164a7d859631cf6b560007003
Alex Brotman <alexbrotman@gmail.com>
2020-02-28 13:34:16 -0600

    Adds config file functionality

--- src/switchboard.js
+++ src/switchboard.js
@@ -3,8 +3,56 @@
 // How to handle purging old url parameters when a new one is used
 //// Perhaps concat the url parameter with 'switchboard_' for easy identification
 
+// Wait for the DOM to fully load
 document.onreadystatechange = function() {
   if (document.readyState === "complete") {
-    console.log("DOM loaded");
+    retrieveConfig(loadConfig);
   }
 };
+
+var _switchboard = {};
+
+// Set config defaults
+_switchboard.config = {
+  url_parameter: "switchboard",
+  local_storage_expiration: "",
+  clean_up_list: []
+};
+
+// Grab the current path to the JS file
+var __scripts = document.getElementsByTagName("script");
+_switchboard.script_dir = getRelativePath();
+
+// Determine the path to the current script
+function getRelativePath() {
+  var scripts = document.getElementsByTagName("script");
+  var absolutePath = scripts[scripts.length - 1].src;
+  var filename_split = absolutePath.split("/");
+  filename_split.pop();
+  return filename_split.join("/");
+}
+
+// Retrieve the config settings from switchboard_config.json
+function retrieveConfig(callback) {
+  var request = new XMLHttpRequest();
+  request.overrideMimeType("application/json");
+  request.open(
+    "GET",
+    _switchboard.script_dir + "/switchboard_config.json",
+    true
+  );
+  request.onreadystatechange = function() {
+    if (request.readyState == 4 && request.status == "200") {
+      callback(request.responseText);
+    }
+  };
+  request.send(null);
+}
+
+// Load the config settings
+function loadConfig(json_config) {
+  var config = JSON.parse(json_config);
+  for (var property in config) {
+    _switchboard.config[property] = config[property];
+  }
+}
